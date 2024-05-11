a1f29e8f06c088fa4e6684ad03244fe0f7a63f0f
Chris <cristian.28hedz@gmail.com>
2024-05-11 17:02:28 -0600

    init: 🎉 commit inicial

---
+++ src/main/java/com/chris/courseplatform/app/CoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.chris.courseplatform.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursePlatformApplication.class, args);
+    }
+
+}
