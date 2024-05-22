3ac47d362124ff69f80fe4f66cf93625ddf6a9ab
Chris <cristian.28hedz@gmail.com>
2024-05-21 18:31:49 -0600

    feat: 🚀 agregue auditoria para agregar las fechas de creacion y actualizacion de un curso

--- src/main/java/com/chris/courseplatform/app/models/Course.java
+++ src/main/java/com/chris/courseplatform/app/models/Course.java
@@ -2,10 +2,19 @@ package com.chris.courseplatform.app.models;
 
 import com.chris.courseplatform.app.models.Dto.Category;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Set;
 
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "courses")
 public class Course {
@@ -18,10 +27,13 @@ public class Course {
     private Category category;
     private BigDecimal price;
     private String image;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "instructor_id")
     private User instructor;
-    private String duration;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Lesson> lessons;
@@ -33,5 +45,5 @@ public class Course {
     private Set<Question> questions;
 
     @ManyToMany(mappedBy = "enrolledCourses")
-    private Set<User> estudiantes;
+    private Set<User> students;
 }
